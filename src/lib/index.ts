2b15614dbc31e6d9ecef025549e073db045d4886
kerusu <kerusu1017@yahoo.co.jp>
2023-08-14 15:33:58 +0900

    init

---
+++ src/lib/index.ts
@@ -0,0 +1,18 @@
+// place files you want to import through the `$lib` alias in this folder.
+const nextExps = [
+	0, 54, 71, 108, 128, 164, 202, 244, 274, 315, 345, 376, 407, 419, 429, 440, 454, 469, 483, 497,
+	515, 537, 558, 579, 600
+];
+
+export const calcRequiredExp = (currentLevel: number, targetLevel: number): number => {
+	let requiredExp: number = 0;
+	for (let i = currentLevel; i < targetLevel; i++) {
+		requiredExp += nextExps[i];
+	}
+	return requiredExp;
+};
+
+export const calcRequiredCandy = (exp: number): number => {
+	const candyExp = 25;
+	return Math.ceil(exp / candyExp);
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,106 @@
+<script lang="ts">
+	import { calcRequiredExp, calcRequiredCandy } from '$lib/index';
+	const minLevel = 1;
+	const maxLevel = 25;
+	let currentLevel = 1;
+	let targetLevel = 25;
+	$: requiredExp = calcRequiredExp(currentLevel, targetLevel);
+	$: requiredCandy = calcRequiredCandy(requiredExp);
+</script>
+
+<h1>Pokémon Sleep アメ・経験値シミュレータ(25レベルまで対応)</h1>
+<div class="input-level">
+	<div>
+		<h2>現在のレベル</h2>
+		<div class="counter">
+			<button
+				on:click={() => {
+					if (currentLevel > minLevel) currentLevel -= 1;
+				}}
+			>
+				<svg aria-hidden="true" viewBox="0 0 1 1">
+					<path d="M0,0.5 L1,0.5" />
+				</svg>
+			</button>
+			<strong>{currentLevel}</strong>
+			<button
+				on:click={() => {
+					if (currentLevel < maxLevel) currentLevel += 1;
+				}}
+			>
+				<svg aria-hidden="true" viewBox="0 0 1 1">
+					<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
+				</svg>
+			</button>
+		</div>
+	</div>
+	<div>
+		<h2>目標レベル</h2>
+		<div class="counter">
+			<button
+				on:click={() => {
+					if (targetLevel > minLevel) targetLevel -= 1;
+				}}
+			>
+				<svg aria-hidden="true" viewBox="0 0 1 1">
+					<path d="M0,0.5 L1,0.5" />
+				</svg>
+			</button>
+			<strong>{targetLevel}</strong>
+			<button
+				on:click={() => {
+					if (targetLevel < maxLevel) targetLevel += 1;
+				}}
+			>
+				<svg aria-hidden="true" viewBox="0 0 1 1">
+					<path d="M0,0.5 L1,0.5 M0.5,0 L0.5,1" />
+				</svg>
+			</button>
+		</div>
+	</div>
+</div>
+<div>必要経験値: {requiredExp.toLocaleString()}</div>
+<div>アメの数: {requiredCandy} 個</div>
+<div>
+	<h2>注意:</h2>
+	<ul>
+		<li>このシミュレータは非公式のものです。</li>
+		<li>性格による獲得経験値補正には対応していません。</li>
+		<li>一部の経験値テーブルが異なるポケモンには対応していません。</li>
+	</ul>
+</div>
+
+<style>
+	.input-level {
+		display: flex;
+		justify-content: space-evenly;
+	}
+
+	.counter {
+		display: flex;
+		margin: 1rem 0;
+	}
+
+	.counter button {
+		width: 2em;
+		padding: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 0;
+		background-color: transparent;
+		touch-action: manipulation;
+		font-size: 2rem;
+	}
+
+	svg {
+		width: 25%;
+		height: 25%;
+	}
+
+	path {
+		vector-effect: non-scaling-stroke;
+		stroke-width: 2px;
+		stroke: #444;
+	}
+</style>
