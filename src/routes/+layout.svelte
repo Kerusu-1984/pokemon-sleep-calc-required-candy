d2f8eabf321458183afc9ffe097d3efa3f28c966
Kerusu <kerusu1017@yahoo.co.jp>
2023-09-19 05:39:18 +0000

    フォント導入

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 </script>
 
-<div data-theme="cupcake">
+<div data-theme="light">
 	<div class="navbar bg-base-200">
 		<a href="/" class="text-xl"> アメ何個いる？(Pokémon Sleep アメ・経験値シミュレータ)</a>
 	</div>
