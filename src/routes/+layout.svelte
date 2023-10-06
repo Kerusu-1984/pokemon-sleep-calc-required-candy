ddfe5e8058e07c2037c860eba1448479edd0a677
Kerusu <kerusu1017@yahoo.co.jp>
2023-10-06 15:00:46 +0000

    英語から日本語に

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
 <div data-theme="emerald">
 	<div class="navbar bg-base-200">
-		<div class="text-xl">アメ何個いる？(Pokémon Sleep アメ・経験値シミュレータ)</div>
+		<div class="text-xl">アメ何個いる？(ポケモンスリープ アメ・経験値シミュレータ)</div>
 	</div>
 	<div class="max-w-6xl mx-auto p-4 sm:p-6 md:p-8">
 		<slot />
