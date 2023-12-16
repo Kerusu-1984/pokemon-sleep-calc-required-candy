8add6bc931c024d039707cde10b70bbbba8f1ea0
Kerusu <kerusu1017@yahoo.co.jp>
2023-12-16 06:39:44 +0000

    ゆめのかけらの計算を追加

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,12 @@ const nextExps = [
 	893, 910, 928, 945, 963, 980, 997, 1015, 1032, 1049, 1066
 ];
 
+const dreamShardsPerCandy = [
+	0, 14, 18, 22, 27, 30, 34, 39, 44, 48, 50, 52, 53, 56, 59, 62, 66, 68, 71, 74, 78, 81, 85, 88, 92,
+	95, 100, 105, 111, 117, 122, 126, 130, 136, 143, 151, 160, 167, 174, 184, 192, 201, 211, 221, 227,
+	236, 250, 264, 279, 295
+];
+
 const baseExp = 25;
 
 export type ExpTable = '600' | '900';
@@ -24,13 +30,33 @@ export const calcRequiredCandy = (exp: number, candyExp: number): number => {
 	return Math.ceil(exp / candyExp);
 };
 
-export const calcCandyExp = (nature: number, isBoosted = false): number => {
+export const calcCandyExp = (nature: number, isBoosted: boolean = false): number => {
 	const boost = isBoosted ? 2 : 1;
 	return Math.round(baseExp * nature * boost);
-}
-export const adjustRequiredExpWithExpTable = (exp: number, expTable: ExpTable) => {
+};
+
+export const calcRequiredDreamShards = (
+	currentLevel: number,
+	targetLevel: number,
+	expTable: ExpTable,
+	candyExp: number,
+	isBoosted: boolean = false
+): number => {
+	let dreamShardsSum = 0;
+	let currentExp = 0;
+	for (let level = currentLevel; level < targetLevel; level++) {
+		const requiredExp = adjustRequiredExpWithExpTable(nextExps[level], expTable);
+		const expDiff = requiredExp - currentExp;
+		const dreamShards = Math.ceil(expDiff / candyExp) * dreamShardsPerCandy[level];
+		dreamShardsSum += dreamShards;
+		currentExp = expDiff % candyExp == 0 ? 0 : candyExp - (expDiff % candyExp);
+	}
+	return isBoosted ? dreamShardsSum * 6 : dreamShardsSum;
+};
+
+export const adjustRequiredExpWithExpTable = (exp: number, expTable: ExpTable): number => {
 	if (expTable === '900') {
 		return Math.ceil(exp * 1.5);
 	}
 	return exp;
-}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
-	import { calcRequiredExp, calcRequiredCandy,calcCandyExp, type ExpTable } from '$lib/index';
+	import {
+		calcRequiredExp,
+		calcRequiredCandy,
+		calcCandyExp,
+		calcRequiredDreamShards,
+		type ExpTable
+	} from '$lib/index';
 	import LevelInput from './LevelInput.svelte';
 	let currentLevel = 10;
 	let targetLevel = 25;
 	let expTable: ExpTable = '600';
 	let natureMagnification = 1;
 	let isBoosted = false;
-	$: candyExp = calcCandyExp(natureMagnification,isBoosted);
+	$: candyExp = calcCandyExp(natureMagnification, isBoosted);
 	$: requiredExp = calcRequiredExp(currentLevel, targetLevel, expTable);
+	$: requiredDreamShards = calcRequiredDreamShards(
+		currentLevel,
+		targetLevel,
+		expTable,
+		candyExp,
+		isBoosted
+	);
 	$: requiredCandy = calcRequiredCandy(requiredExp, candyExp);
 </script>
 
@@ -17,71 +30,80 @@
 </div>
 
 <div class="mt-8">
-<div class="max-w-sm my-4 flex mx-auto justify-between content-center">
-	<div class="h-fit my-auto">経験値タイプ</div>
-	<div class="join">
+	<div class="max-w-sm my-4 flex mx-auto justify-between content-center">
+		<div class="h-fit my-auto">経験値タイプ</div>
+		<div class="join">
+			<input
+				class="join-item btn btn-sm"
+				type="radio"
+				name="exp-table"
+				aria-label="600"
+				checked
+				on:change={() => (expTable = '600')}
+			/>
+			<input
+				class="join-item btn btn-sm"
+				type="radio"
+				name="exp-table"
+				aria-label="900"
+				on:change={() => (expTable = '900')}
+			/>
+		</div>
+	</div>
+
+	<div class="max-w-sm my-4 flex mx-auto justify-between content-center">
+		<div class="h-fit my-auto">性格による経験値補正</div>
+		<div class="join">
+			<input
+				class="join-item btn btn-sm"
+				type="radio"
+				name="options"
+				aria-label="▼"
+				on:change={() => (natureMagnification = 0.82)}
+			/>
+			<input
+				class="join-item btn btn-sm"
+				type="radio"
+				name="options"
+				aria-label="-"
+				checked
+				on:change={() => (natureMagnification = 1)}
+			/>
+			<input
+				class="join-item btn btn-sm"
+				type="radio"
+				name="options"
+				aria-label="▲"
+				on:change={() => (natureMagnification = 1.18)}
+			/>
+		</div>
+	</div>
+
+	<div class="max-w-sm my-4 flex mx-auto justify-between content-center">
+		<div class="h-fit my-auto">
+			アメブースト
+			<label class="btn btn-circle btn-xs" for="candy_boost_modal">?</label>
+		</div>
 		<input
-			class="join-item btn btn-sm"
-			type="radio"
-			name="exp-table"
-			aria-label="600"
-			checked
-			on:change={() => (expTable = '600')}
-		/>
-		<input
-			class="join-item btn btn-sm"
-			type="radio"
-			name="exp-table"
-			aria-label="900"
-			on:change={() => (expTable = '900')}
+			type="checkbox"
+			class="toggle toggle-primary"
+			bind:checked={isBoosted}
+			on:click={() => {
+				isBoosted = !isBoosted;
+			}}
 		/>
 	</div>
-</div>
 
-<div class="max-w-sm my-4 flex mx-auto justify-between content-center">
-	<div class="h-fit my-auto">性格による経験値補正</div>
-	<div class="join">
-		<input
-			class="join-item btn btn-sm"
-			type="radio"
-			name="options"
-			aria-label="▼"
-			on:change={() => (natureMagnification=0.82)}
-		/>
-		<input
-			class="join-item btn btn-sm"
-			type="radio"
-			name="options"
-			aria-label="-"
-			checked
-			on:change={() => (natureMagnification=1)}
-		/>
-		<input
-		class="join-item btn btn-sm"
-		type="radio"
-		name="options"
-		aria-label="▲"
-		on:change={() => (natureMagnification=1.18)}
-	/>
+	<input type="checkbox" id="candy_boost_modal" class="modal-toggle" />
+	<div class="modal" role="dialog">
+		<div class="modal-box">
+			<p class="py-4">
+				アメブーストは2023年12月18日~12月25日に実装される予定の機能です。シミュレーションと実際の機能が異なる可能性があります。
+			</p>
+		</div>
+		<label class="modal-backdrop" for="candy_boost_modal">Close</label>
 	</div>
 </div>
-
-<div class="max-w-sm my-4 flex mx-auto justify-between content-center">
-	<div class="h-fit my-auto">アメブースト
-		<label class="btn btn-circle btn-xs" for="candy_boost_modal">?</label>
-	</div>
-	<input type="checkbox" class="toggle toggle-primary" bind:checked={isBoosted} on:click={()=>{isBoosted=!isBoosted}} />
-</div>
-
-<input type="checkbox" id="candy_boost_modal" class="modal-toggle" />
-<div class="modal" role="dialog">
-  <div class="modal-box">
-    <p class="py-4">アメブーストは2023年12月18日~12月25日に実装される予定の機能です。シミュレーションと実際の機能が異なる可能性があります。</p>
-  </div>
-  <label class="modal-backdrop" for="candy_boost_modal">Close</label>
-</div>
-</div>
-
 
 <div class="divider" />
 
@@ -92,12 +114,26 @@
 			alt="アメ"
 			class="w-8 h-8 inline-block"
 		/>
-		{requiredCandy} 個
+		{requiredCandy.toLocaleString()} 個
 	</div>
-	<div>
-		<span class="text-sm mr-4 text-gray-500">目標のレベルまであと</span>
-		<span class="text-xs text-green-500">EXP</span>
-		{requiredExp.toLocaleString()}
+	<div class="mt-2">
+		<div>
+			<span class="text-sm mr-4 text-gray-500">ゆめのかけら</span>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="w-3 h-3 text-warning fill-current inline-block"
+				viewBox="0 0 512 512"
+				><path
+					d="M284.3 11.7c-15.6-15.6-40.9-15.6-56.6 0l-216 216c-15.6 15.6-15.6 40.9 0 56.6l216 216c15.6 15.6 40.9 15.6 56.6 0l216-216c15.6-15.6 15.6-40.9 0-56.6l-216-216z"
+				/></svg
+			>
+			{requiredDreamShards.toLocaleString()}
+		</div>
+		<div>
+			<span class="text-sm mr-4 text-gray-500">目標のレベルまであと</span>
+			<span class="text-xs text-green-500">EXP</span>
+			{requiredExp.toLocaleString()}
+		</div>
 	</div>
 </div>
 
