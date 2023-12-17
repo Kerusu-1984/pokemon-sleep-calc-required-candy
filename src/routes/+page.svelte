2c46dae58577b26a90c5ae8fa3ecee1c6e14b1d4
Kerusu <kerusu1017@yahoo.co.jp>
2023-12-17 19:27:34 +0000

    アメブーストの注意書きを削除

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -94,15 +94,6 @@
 		/>
 	</div>
 
-	<input type="checkbox" id="candy_boost_modal" class="modal-toggle" />
-	<div class="modal" role="dialog">
-		<div class="modal-box">
-			<p class="py-4">
-				アメブーストは2023年12月18日~12月25日に実装される予定の機能です。シミュレーションと実際の機能が異なる可能性があります。
-			</p>
-		</div>
-		<label class="modal-backdrop" for="candy_boost_modal">Close</label>
-	</div>
 </div>
 
 <div class="divider" />
