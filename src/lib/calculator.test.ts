f209a738578dc27b3a24c7b2908954608d69a361
kerusu <kerusu1017@yahoo.co.jp>
2024-04-09 14:40:01 +0900

    計算機リファクタ

---
+++ src/lib/calculator.test.ts
@@ -0,0 +1,66 @@
+import {
+	calcBoostedCandyExp,
+	calcCandyExp,
+	calcRequiredBoostedCandy,
+	calcRequiredBoostedDreamShards,
+	calcRequiredCandy,
+	calcRequiredDreamShards,
+	calcRequiredExp,
+	calcRequiredMiniBoostedDreamShards
+} from './calculator';
+
+test('exp', () => {
+	expect(calcRequiredExp(24, 25, '600')).toBe(600);
+	expect(calcRequiredExp(20, 21, '600')).toBe(515);
+	expect(calcRequiredExp(1, 2, '600')).toBe(54);
+	expect(calcRequiredExp(20, 22, '600')).toBe(1052);
+	expect(calcRequiredExp(19, 21, '600')).toBe(1012);
+	expect(calcRequiredExp(20, 23, '600')).toBe(1610);
+	expect(calcRequiredExp(19, 22, '600')).toBe(1549);
+	expect(calcRequiredExp(20, 24, '600')).toBe(2189);
+	expect(calcRequiredExp(2, 3, '900')).toBe(107);
+	expect(calcRequiredExp(9, 10, '900')).toBe(472);
+	expect(calcRequiredExp(10, 11, '900')).toBe(518);
+	expect(calcRequiredExp(10, 12, '900')).toBe(1082);
+	expect(calcRequiredExp(24, 25, '1080')).toBe(1080);
+});
+
+test('candyExp', () => {
+	expect(calcCandyExp('normal')).toBe(25);
+	expect(calcCandyExp('down')).toBe(21);
+	expect(calcCandyExp('up')).toBe(30);
+	expect(calcBoostedCandyExp('normal')).toBe(50);
+	expect(calcBoostedCandyExp('down')).toBe(42);
+	expect(calcBoostedCandyExp('up')).toBe(60);
+});
+
+test('candy', () => {
+	expect(calcRequiredCandy(24, 25, 'normal', '600')).toBe(24);
+	expect(calcRequiredCandy(24, 25, 'normal', '900')).toBe(36);
+	expect(calcRequiredCandy(24, 25, 'normal', '1080')).toBe(44);
+	expect(calcRequiredCandy(24, 25, 'down', '600')).toBe(29);
+	expect(calcRequiredCandy(24, 25, 'down', '900')).toBe(43);
+	expect(calcRequiredCandy(24, 25, 'down', '1080')).toBe(52);
+	expect(calcRequiredCandy(24, 25, 'up', '600')).toBe(20);
+	expect(calcRequiredCandy(24, 25, 'up', '900')).toBe(30);
+	expect(calcRequiredCandy(24, 25, 'up', '1080')).toBe(36);
+	expect(calcRequiredBoostedCandy(24, 25, 'normal', '600')).toBe(12);
+	expect(calcRequiredBoostedCandy(24, 25, 'normal', '900')).toBe(18);
+	expect(calcRequiredBoostedCandy(24, 25, 'normal', '1080')).toBe(22);
+	expect(calcRequiredBoostedCandy(24, 25, 'down', '600')).toBe(15);
+	expect(calcRequiredBoostedCandy(24, 25, 'down', '900')).toBe(22);
+	expect(calcRequiredBoostedCandy(24, 25, 'down', '1080')).toBe(26);
+	expect(calcRequiredBoostedCandy(24, 25, 'up', '600')).toBe(10);
+	expect(calcRequiredBoostedCandy(24, 25, 'up', '900')).toBe(15);
+	expect(calcRequiredBoostedCandy(24, 25, 'up', '1080')).toBe(18);
+});
+
+test('dreamShards', () => {
+	expect(calcRequiredDreamShards(24, 25, 'normal', '600')).toBe(2208);
+	expect(calcRequiredDreamShards(25, 26, 'normal', '600')).toBe(2375);
+	expect(calcRequiredDreamShards(20, 21, 'normal', '600')).toBe(1638);
+	expect(calcRequiredDreamShards(20, 22, 'normal', '600')).toBe(3420);
+	expect(calcRequiredDreamShards(20, 23, 'normal', '600')).toBe(5290);
+	expect(calcRequiredBoostedDreamShards(24, 25, 'normal', '600')).toBe(6624);
+	expect(calcRequiredMiniBoostedDreamShards(24, 25, 'normal', '600')).toBe(4416);
+});
---
+++ src/lib/calculator.ts
@@ -0,0 +1,145 @@
+const totalExps = [
+	0, 54, 125, 233, 361, 525, 727, 971, 1245, 1560, 1905, 2281, 2688, 3107, 3536, 3976, 4430, 4899,
+	5382, 5879, 6394, 6931, 7489, 8068, 8668, 9290, 9933, 10598, 11284, 11992, 12721, 13469, 14235,
+	15020, 15823, 16644, 17483, 18340, 19215, 20108, 20108, 21018, 21946, 22891, 23854, 24834, 25831,
+	26846, 27878, 28927, 29993, 31355, 32917, 34664, 36610, 38805
+];
+
+const dreamShardsPerCandy = [
+	0, 14, 18, 22, 27, 30, 34, 39, 44, 48, 50, 52, 53, 56, 59, 62, 66, 68, 71, 74, 78, 81, 85, 88, 92,
+	95, 100, 105, 111, 117, 122, 126, 130, 136, 143, 151, 160, 167, 174, 184, 192, 201, 211, 221, 227,
+	236, 250, 264, 279, 295, 309, 323, 338, 356, 372
+];
+
+const expTypeToRatio = {
+	'600': 1,
+	'900': 1.5,
+	'1080': 1.8
+};
+
+export type ExpType = keyof typeof expTypeToRatio;
+
+const natureToCandyExp = {
+	down: 21,
+	normal: 25,
+	up: 30
+};
+
+export type Nature = keyof typeof natureToCandyExp;
+
+export type Event = 'none' | 'boosted' | 'miniBoosted';
+
+export const calcRequiredExp = (currentLevel: number, targetLevel: number, expType: ExpType) => {
+	if (currentLevel >= targetLevel) {
+		return 0;
+	}
+	const ratio = expTypeToRatio[expType];
+	const currentExp = Math.round(totalExps[currentLevel - 1] * ratio);
+	const targetExp = Math.round(totalExps[targetLevel - 1] * ratio);
+	return targetExp - currentExp;
+};
+
+export const calcCandyExp = (nature: Nature) => {
+	return natureToCandyExp[nature];
+};
+
+export const calcBoostedCandyExp = (nature: Nature) => {
+	return natureToCandyExp[nature] * 2;
+};
+export const calcRequiredCandy = (
+	currentLevel: number,
+	targetLevel: number,
+	nature: Nature,
+	expType: ExpType
+) => {
+	const requiredExp = calcRequiredExp(currentLevel, targetLevel, expType);
+	const candyExp = calcCandyExp(nature);
+	return Math.ceil(requiredExp / candyExp);
+};
+
+export const calcRequiredBoostedCandy = (
+	currentLevel: number,
+	targetLevel: number,
+	nature: Nature,
+	expType: ExpType
+) => {
+	const requiredExp = calcRequiredExp(currentLevel, targetLevel, expType);
+	const candyExp = calcBoostedCandyExp(nature);
+	return Math.ceil(requiredExp / candyExp);
+};
+
+export const calcRequiredDreamShards = (
+	currentLevel: number,
+	targetLevel: number,
+	nature: Nature,
+	expType: ExpType
+) => {
+	let dreamShardsSum = 0;
+	let carry = 0;
+	for (let level = currentLevel; level < targetLevel; level++) {
+		const requiredExp = calcRequiredExp(level, level + 1, expType) - carry;
+		const requiredcandy = Math.ceil(requiredExp / calcCandyExp(nature));
+		dreamShardsSum += dreamShardsPerCandy[level] * requiredcandy;
+		carry = calcCandyExp(nature) * requiredcandy - calcRequiredExp(level, level + 1, expType);
+	}
+	return dreamShardsSum;
+};
+
+export const calcRequiredBoostedDreamShards = (
+	currentLevel: number,
+	targetLevel: number,
+	nature: Nature,
+	expType: ExpType
+) => {
+	let dreamShardsSum = 0;
+	let carry = 0;
+	for (let level = currentLevel; level < targetLevel; level++) {
+		const requiredExp = calcRequiredExp(level, level + 1, expType) - carry;
+		const requiredcandy = Math.ceil(requiredExp / calcBoostedCandyExp(nature));
+		dreamShardsSum += dreamShardsPerCandy[level] * requiredcandy * 6;
+		carry =
+			calcBoostedCandyExp(nature) * requiredcandy - calcRequiredExp(level, level + 1, expType);
+	}
+	return dreamShardsSum;
+};
+
+export const calcRequiredMiniBoostedDreamShards = (
+	currentLevel: number,
+	targetLevel: number,
+	nature: Nature,
+	expType: ExpType
+) => {
+	let dreamShardsSum = 0;
+	let carry = 0;
+	for (let level = currentLevel; level < targetLevel; level++) {
+		const requiredExp = calcRequiredExp(level, level + 1, expType) - carry;
+		const requiredcandy = Math.ceil(requiredExp / calcBoostedCandyExp(nature));
+		dreamShardsSum += dreamShardsPerCandy[level] * requiredcandy * 4;
+		carry =
+			calcBoostedCandyExp(nature) * requiredcandy - calcRequiredExp(level, level + 1, expType);
+	}
+	return dreamShardsSum;
+};
+
+export const Calculator = (event: Event) => {
+	switch (event) {
+		case 'none':
+			return {
+				calcRequiredCandy,
+				calcRequiredDreamShards,
+				calcRequiredExp
+			};
+		case 'boosted':
+			return {
+				calcRequiredCandy: calcRequiredBoostedCandy,
+				calcRequiredDreamShards: calcRequiredBoostedDreamShards,
+				calcRequiredExp
+			};
+		case 'miniBoosted':
+			return {
+				calcRequiredCandy: calcRequiredBoostedCandy,
+				calcRequiredDreamShards: calcRequiredMiniBoostedDreamShards,
+				calcRequiredExp
+			};
+	}
+};
